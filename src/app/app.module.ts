9d0efc04be463ef862b05d061c7817bd2d0e721c
naromano <naromano95@gmail.com>
2023-03-01 11:53:38 -0300

    empece formulario crear post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { CKEditorModule } from 'ng2-ckeditor';
 
 
 
--- src/app/components/blog/create-post/create-post.component.ts
+++ src/app/components/blog/create-post/create-post.component.ts
@@ -1,29 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit, ViewChild } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { CKEditorComponent } from 'ng2-ckeditor';
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.css']
 })
-export class CreatePostComponent implements OnInit {
-  
-  constructor (private fb: UntypedFormBuilder, private sanitizer: DomSanitizer) {}
+export class CreatePostComponent{
 
-  ngOnInit(): void {
-
+  constructor (private fb: UntypedFormBuilder, private sanitizer: DomSanitizer) {
+    
   }
   name: string = "holaaa"
   renderizado: SafeHtml = ''
   htmlCode = '<h1>Hello World!</h1>';
 
   myForm: UntypedFormGroup = this.fb.group({
+    title: ['', [Validators.required]],
     html: ['', [Validators.required]],
+    img: ['', [Validators.required]],
   })
 
   mostrar(){
     this.name = this.myForm.get("html")?.value
+    console.log(this.myForm.get("title")?.value)
     
 
     this.renderizado = this.sanitizer.bypassSecurityTrustHtml(this.name);
